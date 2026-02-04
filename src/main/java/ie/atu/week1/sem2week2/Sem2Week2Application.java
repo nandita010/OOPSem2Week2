302db99f631bcb9f82175f0d23ba2158c24b684b
Nandita <g00463584@atu.ie>
2026-02-04 04:59:59 +0000

    Created Project

---
+++ src/main/java/ie/atu/week1/sem2week2/Sem2Week2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week1.sem2week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2Week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem2Week2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week1/sem2week2/Sem2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week1.sem2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
